4bc182eb87e47346c25de20926cac5acfc71bdbb
gracekeane <gracekeane82@gmail.com>
2021-04-18 12:15:20 +0100

    Initialized fingers and added lines and center point between two fingers

--- VolumeControl.py
+++ VolumeControl.py
@@ -15,7 +15,8 @@ cap.set(4, hCam)
 # Previous time
 pTime = 0
 
-detector = tm.handDetector()
+# Only detect the hand if very certain it is a hand
+detector = tm.handDetector(detectionCon=0.9)
 
 
 # Check success
@@ -23,6 +24,30 @@ while True:
     success, img = cap.read()
     # Send open cv  image to detector
     img = detector.findHands(img)
+    # Get position
+    lmList = detector.findPosition(img, draw=False)
+
+    if len(lmList) != 0:
+        # Getting landmark 4 and 8 -> var for top of thumb and
+        # index finger
+        print(lmList[4], lmList[8])
+        # Create a circle around the two fingers
+
+        # Get x and y positions of two fingers
+        x1, y1 = lmList[4][1], lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+        # Get center of the line
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        # Take in img, position, radius, color, fill
+        cv2.circle(img, (x1, y1), 15, (255, 0, 0), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 15, (255, 0, 0), cv2.FILLED)
+        # Create a line between the two fingers
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+        # Put circle around the middle
+        cv2.circle(img, (cx, cy), 15, (255, 0, 0), cv2.FILLED)
+
 
 
 
